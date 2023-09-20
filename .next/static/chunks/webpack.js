964fcddf4abe5fab273e8debb1e110ab7493b51d
김진영 <kimjin-young@gimjin-yeong-ui-MacBookAir.local>
2023-09-21 03:02:02 +0900

    feat : 메인페이지의 랭킹, 나의 mbti, 출석잔디, 뱃지 컴포넌트 생성

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6fdbf02b93070474"; }
+/******/ 		__webpack_require__.h = function() { return "4f5ac13e6d1ecd5c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,35 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
-import styles from '@/styles/Home.module.css';
+import styles from '@/styles/Home.module.scss';
 import Sidebar from '@/layout/Sidebar';
-import AttendanceJandi from '@/components/statistics/AttendanceJandi';
-
 import dynamic from 'next/dynamic';
 import Ranking from '@/components/statistics/Ranking';
+import Link from 'next/link';
+import { IoIosArrowForward } from 'react-icons/io';
+import badge from '../public/image/badge_01.png';
 
 const MyResponsivePie = dynamic(() => import('../components/statistics/AttendanceJandi'), { ssr: false });
+const MyResponsiveRadar = dynamic(() => import('../components/statistics/CodingMBTI'), { ssr: false });
 
 export default function Home() {
     return (
-        <div>
+        <section className={styles.container}>
             {/* <Sidebar /> */}
-            {/* <MyResponsivePie /> */}
             <Ranking />
-        </div>
+            <MyResponsiveRadar />
+            <MyResponsivePie />
+            <article className={styles.ly_col_30}>
+                <h2>획득한 뱃지</h2>
+                <Link href={'/'} className={styles.moreInfo}>
+                    자세히 보기 <IoIosArrowForward />
+                </Link>
+                <div className={`${styles.contentBox} ${styles.badgeBox}`}>
+                    <p>
+                        <span>8</span> 개 / 30
+                    </p>
+                    <Image src={badge} alt="배지바로가기" width={100} height={100} />
+                </div>
+            </article>
+        </section>
     );
 }
